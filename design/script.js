1086a1f997afb13f91d8ee99a7e91b4c39f10aee
Stephen-254 <stephenmutheu@gmail.com>
2024-05-28 21:28:27 +0300

    The paragraphs are selected from JS

---
+++ design/script.js
@@ -0,0 +1,5 @@
+let firstParagraph = document.getElementById("para-one");
+let secondParagraph = document.getElementById("para-two");
+let thirdParagraph = document.getElementById("para-three");
+let fourthParagraph = document.getElementById("para-four");
+
