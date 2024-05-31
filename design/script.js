a0b6998666c4579270fd0f25a6be8b4789f78516
Stephen-254 <stephenmutheu@gmail.com>
2024-05-31 07:53:35 +0300

    Functionality yet to be added

--- design/script.js
+++ design/script.js
@@ -1,5 +1,16 @@
-let firstParagraph = document.getElementById("para-one");
-let secondParagraph = document.getElementById("para-two");
-let thirdParagraph = document.getElementById("para-three");
-let fourthParagraph = document.getElementById("para-four");
+const firstParagraph = document.getElementById("para-one");
+const secondParagraph = document.getElementById("para-two");
+const thirdParagraph = document.getElementById("para-three");
+const fourthParagraph = document.getElementById("para-four");
+const additionSign = document.querySelectorAll("#plus");
+
+
+
+firstParagraph.style.display = "none";
+secondParagraph.style.display = "none";
+thirdParagraph.style.display = "none";
+fourthParagraph.style.display = "none";
+additionSign.style.display = "none";
+
+
 
